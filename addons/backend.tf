a2032f0e9a8465251e5e306158451bbb2a846d86
CelestineOkpataku <celestine.okpataku@gmail.com>
2024-05-14 00:28:47 -0500

    workoo

--- addons/backend.tf
+++ addons/backend.tf
@@ -55,7 +55,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "celestine-okpataku-remote-backend"
-    key            = "okpataku/comcast-assessment-addons/terraform.tfstate"
+    key            = "okpataku/comcast-assessment/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "dynamodb-state-locking"
   }
