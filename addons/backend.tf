bb1ce9d553b6a2a3c3c7da14dbdfbbb70ac0e45f
CelestineOkpataku <celestine.okpataku@gmail.com>
2024-05-14 00:15:31 -0500

    workoo

--- addons/backend.tf
+++ addons/backend.tf
@@ -1,4 +1,4 @@
-
+/*
 provider "kubernetes" {
   host                   = module.eks_blueprints.eks_cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
@@ -19,7 +19,7 @@ provider "helm" {
     token                  = data.aws_eks_cluster_auth.this.token
   }
 }
-*/
+
 
 data "aws_eks_cluster_auth" "this" {
   name = module.eks_blueprints.eks_cluster_id
@@ -42,4 +42,55 @@ terraform {
 
 provider "aws" {
   region = "us-west-2" 
+}
+*/
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"  // Adjust version as needed
+    }
+  }
+  backend "s3" {
+    bucket         = "celestine-okpataku-remote-backend"
+    key            = "okpataku/comcast-assessment-addons/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "dynamodb-state-locking"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"  // Ensure it matches your S3 bucket region
+  profile                 = "default" 
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks_blueprints.eks_cluster_id
+}
+
+provider "kubernetes" {
+  host                   = module.eks_blueprints.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    // This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks_blueprints.eks_cluster_id]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks_blueprints.eks_cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks_blueprints.eks_cluster_id]
+    }
+  }
 }
